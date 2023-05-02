6839ded91c1c0a4e9c982db3701e9d9574d1756a
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-02 20:43:17 -0300

    modify about css

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,26 +4,31 @@ const About = () => {
   return (
     <section className="bg-[#212121] flex justify-center items-center">
       <div className="max-w-xl">
-        <h1 className="text-zinc-100 underline underline-offset-4 decoration-[#55c5e9] text-lg font-bold">
+        <h1 className="mb-3 text-zinc-100 underline underline-offset-4 decoration-[#55c5e9] text-xl font-bold">
           Sobre Mim
         </h1>
-        <h2 className="font-semibold text-lg text-zinc-200 my-4">
+        <div className="flex flex-col gap-4">
+        <p className="font-light text-lg text-zinc-300">
           Estou muito animado em começar minha carreira nesta área e estou
           sempre buscando aprender mais para me tornar um desenvolvedor melhor.
-        </h2>
-        <p className="font-light text-lg text-zinc-400">
-          Tenho um bom conhecimento dos fundamentos e estou sempre buscando
-          aprimorar minhas habilidades. Eu gosto de trabalhar em equipe e estou
-          sempre disposto a ajudar meus colegas desenvolvedores. Eu sou
+        </p>
+        <p className="font-light text-lg text-zinc-300">
+          Tenho um bom conhecimento dos fundamentos. Gosto de trabalhar em equipe e estou
+          sempre disposto a ajudar e ser ajudado pelos meus colegas desenvolvedores.
+        </p>
+        <p className="font-light text-lg text-zinc-300">
+        Eu sou
           apaixonado por desenvolvimento web e estou sempre antenado nas últimas
-          tendências e novidades da indústria. Eu sou especialmente interessado
-          em tecnologias front-end modernas, como ReactJS, Nextjs.
-          No meu tempo livre, eu
+          tendências e novidades da indústria.
+        </p>
+        <p className="font-light text-lg text-zinc-300">
+        No meu tempo livre, eu
           gosto de experimentar novas ferramentas e frameworks para melhorar
           minha eficiência e desenvolvimento de projetos. Eu adoro desafios e
           estou sempre buscando maneiras de melhorar o desempenho e a
           usabilidade dos sites que eu desenvolvo.
         </p>
+        </div>
       </div>
       <div>
         <Image
