ba6401d4a34801e5ab1dede392ce97acffe88425
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-04 20:28:22 -0300

    adiciona animacoes

---
+++ src/app/components/animations/index.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+interface Props {
+  children: React.ReactNode
+}
+
+const FadeUp = ({children}:Props) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div data-aos="fade-up"
+    data-aos-once="true"
+    >
+      {children}
+    </div>
+  )
+}
+
+const FadeLeft = ({children}:Props) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div data-aos="fade-left"
+    data-aos-offset="300"
+    data-aos-once="true"
+    >
+      {children}
+    </div>
+  )
+}
+
+const FadeRight = ({children}:Props) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div data-aos="fade-right"
+    data-aos-offset="300"
+    data-aos-once="true"
+    >
+      {children}
+    </div>
+  )
+}
+
+export { FadeUp, FadeLeft, FadeRight };
