4f967589c6aa35fc6aa80aa7005a82e943db0830
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-04 21:53:34 -0300

    contact component

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,23 +1,34 @@
 const Contact = () => {
   return (
-    <div>
-      <div>
-        <h1>Entre em contato comigo</h1>
-        <p>Caso você tenha identificado potencial em mim ou deseje conversar comigo, por favor, sinta-se à vontade para me enviar uma mensagem. Estou disponível e aberto para conversar.</p>
+    <section className="mt-28">
+      <div className="flex flex-col justify-center items-center gap-6">
+        <div className="flex flex-col justify-center items-center  gap-3">
+          <h1 className="text-[#55c5e9] text-xl font-semibold">
+            Entre em contato comigo
+          </h1>
+          <p className="max-w-xl text-center text-white text-base font-medium">
+            Caso você tenha identificado potencial em mim ou deseje conversar
+            comigo, por favor, sinta-se à vontade para me enviar uma mensagem.
+            Estou disponível e aberto para conversar.
+          </p>
+        </div>
+        <div className="flex gap-3">
+          <button className="bg-[#55c5e9] p-4 rounded-lg shadow-lg">
+            <a href="mailto:vinisaio10@gmail.com">vinisaio10@gmail.com</a>
+          </button>
+          <button className="bg-[#55c5e9] p-4 rounded-lg shadow-lg">
+            <a href="https://www.linkedin.com/in/vinicius-saiao-goncalves/">
+              Linkedin
+            </a>
+          </button>
+          <button className="bg-[#55c5e9] p-4 rounded-lg shadow-lg">
+            <a href="tel:+5521992176099">(21) 99217-6099</a>
+          </button>
+        </div>
       </div>
-      <div>
-        <button>
-          <a href="mailto:vinisaio10@gmail.com">vinisaio10@gmail.com</a>
-        </button>
-        <button>
-          <a href="https://www.linkedin.com/in/vinicius-saiao-goncalves/">Linkedin</a>
-        </button>
-        <button>
-          <a href="tel:+5521992176099">(21) 99217-6099</a>
-        </button>
-      </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
 export default Contact;
+
