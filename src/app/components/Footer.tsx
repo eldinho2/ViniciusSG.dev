325d68e8cd3d26ed1156dac79ba1f5bcf7d6efeb
Vinicius Goncalves <viniciusfod@gmail.com>
2023-05-23 19:25:20 -0300

    feat: add new project

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,37 +1,66 @@
 import Image from "next/image";
 
 const Footer = () => {
-    return (
-      <footer className="flex justify-between items-center mt-24 h-16 bg-neutral-800 text-lg text-zinc-200 xsm:justify-center xsm:mt-14">
+  return (
+    <footer className="flex justify-between items-center mt-24 h-16 bg-neutral-800 text-lg text-zinc-200 xsm:justify-center xsm:mt-14">
       <div className="flex items-center mx-8 font-semibold cursor-pointer hover:underline underline-offset-4 hover:text-white hover:decoration-[#55c5e9] xsm:hidden">
-        <span className="text-[#55c5e9]">
-          G
-        </span>
+        <span className="text-[#55c5e9]">G</span>
         <a href="http://github.com/eldinho2" target="_blank">
-          <h1 className="w-36">
-            Vinicius
-          </h1>
+          <h1 className="w-36">Vinicius</h1>
         </a>
       </div>
       <div className="flex">
         <p className="flex gap-2">
-          Feito com <Image alt="react-icon" src={'/react-svgrepo-com.svg'} width={30} height={30 } className="animate-spin-slow" /> e <span className="text-[#55c5e9]">❤</span>  por <a href="http://github.com/eldinho2" target="_blank" rel="noopener noreferrer">Vinicius</a>
+          Feito com{" "}
+          <Image
+            alt="react-icon"
+            src={"/react-svgrepo-com.svg"}
+            width={30}
+            height={30}
+            className="animate-spin-slow"
+          />{" "}
+          e <span className="text-[#55c5e9]">❤</span> por{" "}
+          <a
+            href="http://github.com/eldinho2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vinicius
+          </a>
         </p>
       </div>
       <nav className="mx-8 xsm:hidden">
         <ul className="flex items-center gap-2 font-medium">
           <li className="cursor-pointer hover:underline underline-offset-4 hover:text-white hover:decoration-[#55c5e9]">
-            <a href="http://github.com/eldinho2" target="_blank" rel="noopener noreferrer">
-              <Image src="/github-svgrepo-com.svg" alt="" width={40} height={40} />
+            <a
+              href="http://github.com/eldinho2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/github-svgrepo-com.svg"
+                alt=""
+                width={40}
+                height={40}
+              />
             </a>
           </li>
           <li className="cursor-pointer hover:underline underline-offset-4 hover:text-white hover:decoration-[#55c5e9]">
-          <a href="https://www.linkedin.com/in/vinicius-saiao-goncalves/" target="_blank" rel="noopener noreferrer">
-              <Image src="/linkedin-svgrepo-com.svg" alt="" width={45} height={45} />
-          </a>
+            <a
+              href="https://www.linkedin.com/in/vinicius-saiao-goncalves/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/linkedin-svgrepo-com.svg"
+                alt=""
+                width={45}
+                height={45}
+              />
+            </a>
           </li>
           <a
-          className=" bg-[#55c5e9] rounded-md p-1 hover:text-[#55c5e9] hover:bg-[#204b5a] border border-[#55c5e9] hover:border-[#55c5e9]"
+            className=" bg-[#55c5e9] rounded-md p-1 hover:text-[#55c5e9] hover:bg-[#204b5a] border border-[#55c5e9] hover:border-[#55c5e9]"
             href="https://drive.google.com/file/d/1zgNqMidDhYOXh6lLTvkqVv2Z2OJghAj-/view?usp=share_link"
             target="_blank"
           >
@@ -40,7 +69,7 @@ const Footer = () => {
         </ul>
       </nav>
     </footer>
-    )   
-}
+  );
+};
 
 export default Footer;
--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -65,6 +65,55 @@ const Portifolio = () => {
         </FadeLeft>
         <FadeRight>
         <div className="flex flex-row-reverse gap-5 p-4 text-white shadow-lg bg-neutral-800 rounded-lg w-[700px] xsm:w-[330px] xsm:flex-col">
+        <div className="overflow-hidden h-[300px]">
+            <a
+              href="https://github.com/eldinho2/E-commerce"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={"/planPlant.jpeg"}
+                alt="planPlant"
+                width={400}
+                height={400}
+                quality={100}
+                className="translate-y-[0%] ease-[cubic-bezier(0.33,0.42,0.15,1.14)] transition-transform duration-[15s] hover:translate-y-[-80%] hover:transition-transform hover:duration-[15s] hover:ease-[cubic-bezier(0.33,0.42,0.15,1.14)]"
+              />
+            </a>
+          </div>
+          <div className="flex justify-center items-center flex-col gap-3">
+            <div>
+              <p className="max-w-xl font-medium text-base">
+                É um E-commerce de Venda de Plantas desenvolvido com React e Redux e CSS Modules. Com design clean, essa loja prioriza a experiência do usuário, facilitando a localização e compra dos produtos que o cliente precisa.
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <span className="text-xl font-black">React</span>
+              <span className="text-xl font-black">Vanila CSS</span>
+            </div>
+            <div className="flex gap-10">
+              <a
+                href="https://github.com/eldinho2/E-commerce"
+                target="_blank"
+                className="flex items-center gap-1 p-1 border-2 border-[#55c5e9] text-base font-semibold rounded-md hover:border-[#21212b] hover:text-[#21212b] text-[#424156] bg-[#55c5e9] "
+              >
+                Code
+                <FaGithubSquare />
+              </a>
+              <a
+                href="https://e-commerce-eldinho2.vercel.app/"
+                target="_blank"
+                className="flex items-center gap-1 p-1 border-2 border-[#55c5e9] text-base font-semibold rounded-md hover:border-[#21212b] hover:text-[#21212b] text-[#424156] bg-[#55c5e9]"
+              >
+                Live Demo
+                <BsBoxArrowUpRight />
+              </a>
+            </div>
+          </div>
+        </div>
+        </FadeRight>
+        <FadeRight>
+        <div className="flex flex-row-reverse gap-5 p-4 text-white shadow-lg bg-neutral-800 rounded-lg w-[700px] xsm:w-[330px] xsm:flex-col">
           <div className="flex items-center justify-center">
             <a
               href="https://kanban-app-zeta-nine.vercel.app/"
